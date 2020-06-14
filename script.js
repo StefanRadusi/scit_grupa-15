ef6cbf8a5c1a9b7512d80433cd42e8c5369100d2
AdelineC96 <adelinacimpan@yahoo.com>
2020-06-14 18:03:41 +0300

    homework done

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log("functions and scope");
+
+const array=["Rock", "Paper", "Scissors"];
+const computerNumber=Math.floor(Math.random()*3);
+const userNumber=Math.floor(Math.random()*3);
+const computerChoice= array[computerNumber];
+const userChoice= array[userNumber];
+
+const result= "Computer choice: " + computerChoice+ " User choice: "+  userChoice +" "
+
+
+function compareResults(computerChoice,userChoice)
+{
+   //Equality
+
+    if(userChoice===computerChoice){
+  return result + "It's a tie";
+    }
+
+   //Check for Rock
+    if (userChoice==="Rock")
+    {
+      if(computerChoice==="Scissors")
+         return result + "Player Wins";
+         else  return result + "Computer Wins";
+   };
+
+
+   // Check for Paper
+    if (userChoice==="Paper") 
+    {
+      if(computerChoice==="Scissors")
+       return result + "Computer Wins";
+       else  return result + "Player Wins";
+    } 
+
+
+   // Check for Scissors
+
+     if (userChoice==="Scissors")
+     {
+        if(computerChoice==="Rock")
+         return result + "Computer Wins";
+         else return result + "Player Wins";
+     }
+
+}
+const gameResult= compareResults(computerChoice,userChoice);
+console.log(gameResult);
