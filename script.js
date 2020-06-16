46e2af2d3a14a1138dd8a353b3212593d2808b10
Ale-we <liss_alexandra@yahoo.com>
2020-06-16 21:10:09 +0300

    homework done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("functions and scope");
+
+let userChoice = prompt('choose one: rock, paper or scissors?');
+let choiceArray = ['paper', 'rock', 'scissors'];
+let randomNumber = Math.floor(Math.random() * 3);
+let computerChoice = choiceArray[randomNumber];
+
+console.log(`Your choice: ${userChoice},  Computer's choice: ${computerChoice}.`);
+
+if (userChoice === computerChoice){
+    console.log("Tie!");
+}else if (userChoice === 'paper' && computerChoice === 'rock'){
+    console.log("You win!");
+}else if (userChoice === 'rock' && computerChoice === 'scissors'){
+    console.log("You win!");
+}else if (userChoice === 'scissors' || computerChoice === 'paper'){
+    console.log("You win!");
+}else if (userChoice === 'scissors' && computerChoice === 'rock'){
+    console.log("You lose!");
+}else if (userChoice === 'rock' && computerChoice === 'paper'){
+    console.log("You lose!");
+}else if (userChoice === 'paper' && computerChoice === 'scissors'){
+    console.log("You lose!");
+}else{
+console.log("Invalid input, try again!");
+}
+// homework done
