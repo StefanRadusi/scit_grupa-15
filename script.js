1c0c8a4df00812dd6576cb634f3a44a6b2f74c4c
Cati27 <catipreda23@gmail.com>
2020-06-21 16:49:26 +0300

    homework done

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("functions and scope");
+console.log("Preda Catalina - functions and scope");
+
+let array = ["Rock", "Scissors", "Paper"];
+const ComputerChoice = array[Math.floor(array.length * Math.random())];
+const UserChoice = array[Math.floor(array.length * Math.random())];
+
+function playGame() {
+  if (ComputerChoice === UserChoice) {
+    console.log("Computer choice is ${ComputerChoice} ,  User choice is ${UserChoice}. Draw");
+  } else if (ComputerChoice === "Rock" && UserChoice === "Scissors") {
+    console.log("Computer choice is ${ComputerChoice} ,  User choice is ${UserChoice}. Computer wins");
+  } else if (ComputerChoice === "Rock" && UserChoice === "Paper") {
+    console.log("Computer choice is ${ComputerChoice} ,  User choice is ${UserChoice}. User wins");
+  } else if (ComputerChoice === "Paper" && UserChoice === "Rock") {
+    console.log("Computer choice is ${ComputerChoice} ,  User choice is ${UserChoice}. Computer wins");
+  } else if (ComputerChoice === "Paper" && UserChoice === "Scissors") {
+    console.log("Computer choice is ${ComputerChoice} ,  User choice is ${UserChoice}. User wins");
+  } else if (ComputerChoice === "Scissors" && UserChoice === "Rock") {
+    console.log("Computer choice is ${ComputerChoice} ,  User choice is ${UserChoice}. User wins");
+  } else if (ComputerChoice === "Scissors" && UserChoice === "Paper") {
+    console.log("Computer choice is ${ComputerChoice} ,  User choice is ${UserChoice}. Computer wins");
+  }
+}
+playGame();
