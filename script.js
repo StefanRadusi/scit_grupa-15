7f7c97b1c6332aea12dda4549726539b1ca57f00
Diana Brihan <brihan.diana@yahoo.ro>
2020-06-26 18:44:15 +0300

    homework finally done

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
 console.log("start");
 
-// example of reacting to button click
-const sendButton = document.getElementById("send");
 
-// this the function that will be called when click event is triggered
-function onClickSendButton() {
-  console.log("click on", sendButton);
+const addButton = document.getElementById("addFiled");
+
+function onClickAddButton() {
+    const inputField = document.getElementById("fieldName");
+
+    if (inputField.value === "") {
+        inputField.style.borderColor = "red";
+        return
+    }
+
+    inputField.style.borderColor = "";
+    const newInput = document.createElement("input");
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("field");
+    const newPara = document.createElement("p");
+
+    document.querySelector(".form-fields").appendChild(newDiv);
+    newDiv.appendChild(newPara);
+    newDiv.appendChild(newInput);
+
+    newPara.innerText = inputField.value;
+
+    newInput.setAttribute("id", inputField.value);
+    inputField.value = "";
 }
 
-// we attached a function to click event triggered by send button
+addButton.addEventListener("click", onClickAddButton)
+
+
+const sendButton = document.getElementById("send");
+
+function onClickSendButton() {
+
+    let formInputs = document.querySelectorAll("input:not(#fieldName)");
+    let myObject = {};
+    let isComplete = true;
+    for (const field of formInputs) {
+        if (!field.value) {
+            field.style.borderColor = "red";
+            isComplete = false;
+        } else {
+            field.style.borderColor = "";
+        }
+        myObject[field.id] = field.value;
+    }
+
+    // document.getElementsByClassName("form-container")[0].innerHTML = JSON.stringify(myObject);
+    const finalString = Object.keys(myObject).map(function (key) {
+        let count = ` ${key} - ${myObject[key]}`;
+        return count;
+    }).join(", ");
+    if (isComplete === true) {
+        document.getElementsByClassName("form-container")[0].innerHTML = `sended info: ${finalString}`;
+    }
+    console.log(finalString)
+
+    // let inputsValues = Array.from(document.querySelectorAll("input:not(#fieldName)")).map(function (inp) {
+    //     return { idd: inp.id, value: inp.value };
+    // })
+    // // let myObject = {};
+    // for (i = 0; i < inputsValues.length; i++) {
+    //     myObject[inputsValues[i].idd] = inputsValues[i].value;
+    // }
+    // console.log(inputsValue);
+    // console.log(myObject);
+}
+console.log("click on", sendButton);
+
 sendButton.addEventListener("click", onClickSendButton);
