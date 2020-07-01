33f2f4847ee6f57b2190f5db0b151fb207626826
barnutsergiu <barnutsergiu@gmail.com>
2020-07-01 16:28:14 +0300

    finish

--- script.js
+++ script.js
@@ -5,8 +5,29 @@ const sendButton = document.getElementById("send");
 
 // this the function that will be called when click event is triggered
 function onClickSendButton() {
-  console.log("click on", sendButton);
+  console.log("click on", SendButton);
 }
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+// Color change input
+const fildNameBlack= document.getElementById("fieldName")
+function changeColor(){
+if (fildNameBlack.value==="")
+return fildNameBlack.style.borderColor= "red"
+}
+
+addFiled.addEventListener("click", changeColor)
+
+//function display(){
+
+//  a=document.getElementsByClassName("form-fields");
+
+ // b=document.createElement('input')
+ // b.setAttribute("type", "text")
+ // b.setAttribute("id", "newInput")
+
+  //a.appendChild(b);
+//}
+
+//addField.addEventListener("click", display() );
