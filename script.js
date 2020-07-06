f8fab8f20a4c78cd62e1a925512d16f4f8b0320b
Ale-we <liss_alexandra@yahoo.com>
2020-07-06 10:39:32 +0300

    commit

--- script.js
+++ script.js
@@ -13,58 +13,36 @@ sendButton.addEventListener("click", onClickSendButton);
 
 console.log("start homework");
 
-const addButton = document.getElementById("addField");
-
-
-
-//window.onload = function(){
+const addButton = document.getElementById("addFiled");
 
 function onClickAddButton() {
   console.log("click on", addButton);
 
-  //for (const el of addButton){
-  //  const div = document.createElement("div");
-  //  console.log(div);
-  //  div.classList.add("field");
+   const div = document.createElement("div");
+   console.log(div);
+   div.classList.add("field");
 
-  //  const p = document.createElement("p");
-  //  p.innerText = ('{fieldName}');
-  //  div.appendChild(p);
+   const p = document.createElement("p");
+   const nameValue = document.getElementById("fieldName").value;
+   p.innerText = nameValue;
+   div.appendChild(p);
 
-  //  const newInput = document.createElement("input");
-  //  newInput.id = ("{fieldName}");
-  //  div.appendChild(newInput);
+   const newInput = document.createElement("input");
+   newInput.id = ("{fieldName}");
+   div.appendChild(newInput);
 
-  //  document.body.appendChild(div);
-  
- // }
-
-function newInput(){
-  const div = document.createelement("div");
-  div.setAttribute("class" , "field");
-  div.appendChild(newDiv);
-
-  const p = document.createElement("p");
-  p.innerText = newValue.value;
-  div.appendChild(p);
-
-  const input = document.createElement("input");
-  input.setAttribute("id","newField");
-  div.appendChild(input);
-
-  newValue.style.bordercolor="lightgray";
-}
+  document.getElementById("fields").appendChild(div);
+   
  }
-//}
 
 addButton.addEventListener("click", onClickAddButton);
 
 let getNewField = document.getElementById('{fieldName}').value;
 
- if(getNewField.length === 0){
-   getNewField.style.border = "1px solid red";
+ if(newInput.length === ""){
+   newInput.style.border = "1px solid red";
  }else{
-   getNewField.style.border = "1px solid lightgray";
+   newInput.style.border = "1px solid lightgray";
  }
 
 let allFields = document.getElementsByClassName('form-fields').value;
