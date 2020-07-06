214346a2b19296ce0c98d8d6d6ed1a01db0ebbab
AlexEpu <epuranalex89@gmail.com>
2020-07-06 07:20:48 +0300

    homework done

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-console.log("start");
+// console.log("start");
 
-// example of reacting to button click
-const sendButton = document.getElementById("send");
+// // example of reacting to button click
+// // const sendButton = document.getElementById("send");
 
-// this the function that will be called when click event is triggered
-function onClickSendButton() {
-  console.log("click on", sendButton);
+// // this the function that will be called when click event is triggered
+// function onClickSendButton() {
+//   console.log("click on", sendButton);
+// }
+
+// // we attached a function to click event triggered by send button
+// sendButton.addEventListener("click", onClickSendButton);
+
+// console.log("dom manipulation");
+
+
+let AddNewField = document.getElementById("addFiled");
+
+function AddFieldOnClick () {
+    let newFieldName = document.getElementById("fieldName").value;
+        if(newFieldName === null || newFieldName.length === 0 || newFieldName === undefined) {
+            document.getElementById("fieldName").style.borderColor = "red";
+        }
+        else {
+            let newRow = document.createElement("div");
+            document.querySelector(".form-fields").appendChild(newRow);
+            newRow.classList.add("field");
+
+            let newParagraph = document.createElement("p");
+            newRow.appendChild(newParagraph);
+            newParagraph.innerText = document.getElementById("fieldName").value;
+
+            let newInput = document.createElement("input");
+            newRow.appendChild(newInput)
+            newInput.id = newFieldName;
+
+            document.getElementById("fieldName").value = "";
+        }
 }
-
-// we attached a function to click event triggered by send button
-sendButton.addEventListener("click", onClickSendButton);
+AddNewField.addEventListener("click", AddFieldOnClick);
