ecf53e315fa9a12d8e12bab994657a3664012d95
renata2014 <x.rena_x@yahoo.com>
2020-07-08 21:12:10 +0300

    my homework

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function onClickSendButton() {
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+
+const addButton = document.getElementById("addFiled");
+
+function addNewFieldContainer () {
+  const ValueFromInput = document.getElementById("fieldName").value;
+
+  if(ValueFromInput === ""){
+    return document.getElementById("fieldName").style.borderColor = "red";
+  }
+
+  const NewDiv = document.createElement("div");
+  const NewParagraph = document.createElement("p");
+  const NewInput = document.createElement("input");
+  
+  NewDiv.classList.add("field");
+  NewInput.id = ValueFromInput;
+  NewInput.autocomplete = "off";
+  NewParagraph.innerText = ValueFromInput;
+
+  document.getElementById("form-fields-container").appendChild(NewDiv);
+  NewDiv.appendChild(NewParagraph);
+  NewDiv.appendChild(NewInput);
+
+}
+
+addButton.addEventListener("click", addNewFieldContainer);
