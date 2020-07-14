8893cbf7112c45c9763783c712902ed6b97e548a
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-07-14 13:16:48 +0300

    homework clean

---
+++ index.js
@@ -0,0 +1,111 @@
+class Game {
+  constructor() {
+    this.words = ["apple", "car", "house", "university", "sky"];
+    this.lives = 5;
+    this.chooseRandomWord();
+    this.renderTitle();
+    this.renderLetters();
+    this.renderUserInput();
+  }
+
+  chooseRandomWord() {
+    this.currentWord = this.words[
+      Math.floor(Math.random() * this.words.length)
+    ];
+  }
+
+  renderTitle() {
+    const title = document.createElement("h2");
+    title.innerText = "Hangman Game";
+
+    this.renderInitialLives(title);
+
+    document.body.appendChild(title);
+  }
+
+  renderInitialLives(title) {
+    this.livesSpan = document.createElement("span");
+    this.livesSpan.innerText = ` - Lives: ${this.lives}`;
+    title.appendChild(this.livesSpan);
+  }
+
+  updateLives() {
+    this.livesSpan.innerText = ` - Lives: ${this.lives}`;
+  }
+
+  renderLetters() {
+    this.displayLetters = new LettersDisplay(this.currentWord);
+  }
+
+  renderUserInput() {
+    this.input = document.createElement("input");
+    this.input.placeholder = "try a letter";
+    document.body.appendChild(this.input);
+
+    this.input.addEventListener("keyup", this.reactToUserInput.bind(this));
+  }
+
+  reactToUserInput(event) {
+    if (this.input.value.length === 1) {
+      if (event.key === "Enter") {
+        const value = this.input.value;
+
+        if (this.displayLetters.hasLetter(value)) {
+          this.displayLetters.displayLetter(value);
+        } else {
+          this.lives--;
+          this.updateLives();
+        }
+
+        this.input.value = "";
+      }
+    } else if (this.input.value.length !== 0) {
+      this.input.value = event.key;
+    }
+  }
+}
+
+class LettersDisplay {
+  constructor(word) {
+    this.word = word;
+    this.initialDisplay();
+  }
+
+  initialDisplay() {
+    this.lettersContainer = document.createElement("div");
+    this.lettersContainer.classList.add("letters-container");
+    const letters = this.word.split("");
+    this.letters = [];
+    for (const letter of letters) {
+      const letterHtml = document.createElement("p");
+      letterHtml.innerText = "_";
+      this.letters.push({
+        htmlRef: letterHtml,
+        value: letter,
+      });
+      this.lettersContainer.appendChild(letterHtml);
+    }
+
+    document.body.appendChild(this.lettersContainer);
+  }
+
+  hasLetter(letter) {
+    for (const objectLetter of this.letters) {
+      if (objectLetter.value === letter) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  displayLetter(letter) {
+    for (const objectLetter of this.letters) {
+      if (objectLetter.value === letter) {
+        objectLetter.htmlRef.innerText = letter;
+      }
+    }
+  }
+}
+
+new Game();
