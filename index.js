2fdcfd1d3be7ead5d0d5460671df07efd03f4e7e
Diana Brihan <brihan.diana@yahoo.ro>
2020-07-17 16:16:40 +0300

    hangman-homework-helpful-comments

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ class Game {
   }
 
   chooseRandomWord() {
-    this.currentWord = this.words[
-      Math.floor(Math.random() * this.words.length)
-    ];
+    this.currentWord = this.words[Math.floor(Math.random() * this.words.length)];
   }
 
   renderTitle() {
@@ -66,6 +64,7 @@ class Game {
 }
 
 class LettersDisplay {
+  //this.displayLetters = new LettersDisplay(this.currentWord); word-ul dintre paranteze e this.currentWord
   constructor(word) {
     this.word = word;
     this.initialDisplay();
@@ -74,9 +73,9 @@ class LettersDisplay {
   initialDisplay() {
     this.lettersContainer = document.createElement("div");
     this.lettersContainer.classList.add("letters-container");
-    const letters = this.word.split("");
-    this.letters = [];
-    for (const letter of letters) {
+    const letters = this.word.split(""); //const letters e array
+    this.letters = []; // this.letters e o noua entitate, diferita fata de const letters 
+    for (const letter of letters) {  // letters DIFERIT de this.letters !!!!!!!!!!!!!!!
       const letterHtml = document.createElement("p");
       letterHtml.innerText = "_";
       this.letters.push({
@@ -86,10 +85,27 @@ class LettersDisplay {
       this.lettersContainer.appendChild(letterHtml);
     }
 
+    /*
+    this.letters = [
+      {
+        htmlRef: p -> _,
+        value: c
+      },
+      {
+        htmlRef: p -> _,
+        value: a
+      },
+      {
+        htmlRef: p -> _,
+        value : r
+      }
+    ]
+    */
+
     document.body.appendChild(this.lettersContainer);
   }
 
-  hasLetter(letter) {
+  hasLetter(letter) { // const value = this.input.value e letter adica ce litera adauga user-ul in input
     for (const objectLetter of this.letters) {
       if (objectLetter.value === letter) {
         return true;
