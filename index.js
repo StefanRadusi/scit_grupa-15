8220f804ca7ea2f3d73d037cdf625035d12f6c7b
Ale-we <liss_alexandra@yahoo.com>
2020-07-29 10:05:02 +0300

    homework done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class Game {
     this.renderTitle();
     this.renderLetters();
     this.renderUserInput();
+    this.resetButton();
   }
 
   chooseRandomWord() {
@@ -45,6 +46,16 @@ class Game {
     this.input.addEventListener("keyup", this.reactToUserInput.bind(this));
   }
 
+  resetButton () {
+    this.button = document.createElement("button");
+    this.button.id = "btn"
+    this.button.innerText = "Reset"
+    document.body.appendChild(this.button);
+
+    this.button.addEventListener("click", this.resetTheGame.bind(this));
+  
+  }
+
   reactToUserInput(event) {
     if (this.input.value.length === 1) {
       if (event.key === "Enter") {
@@ -52,9 +63,17 @@ class Game {
 
         if (this.displayLetters.hasLetter(value)) {
           this.displayLetters.displayLetter(value);
+
+          if(this.checkIfWin()){
+            this.showWiningState();
+
+          }
         } else {
           this.lives--;
           this.updateLives();
+          if (this.checkIfLoose()){
+            this.showLoosingState();
+          }
         }
 
         this.input.value = "";
@@ -62,6 +81,59 @@ class Game {
     } else if (this.input.value.length !== 0) {
       this.input.value = event.key;
     }
+  }
+
+  checkIfLoose() {
+    if (this.lives === 0) return true;
+
+    return false;
+  }
+
+  showLoosingState(){
+    this.livesSpan.innerText = ":---You lose";
+    this.input.disabled = true;
+    // this.checkIfLoose;
+    // similar with "showWiningState"
+  }
+
+  showWiningState(){
+    this.livesSpan.innerText = ":---You won";
+    this.input.disabled = true;
+    // this.checkIfWin;
+  }
+
+  checkIfWin() {
+    for (const letters of this.displayLetters.letters){
+      if (letters.htmlRef.innerText === "_"){
+        return false;
+
+      } else {
+        // this.showWiningState();
+      return true;
+      }
+     }
+      return false;
+    }
+  
+    //true or false
+    //this.displayLetters.letters(is an array)
+    //for though this.displayLetters.letters, check if htmlRef.innerText === "_" then false
+ 
+
+
+
+  cleanLetters(){
+    this.displayLetters.lettersContainer.remove();
+  }
+
+  //this must be used in reset button
+  resetTheGame(){
+    this.renderLetters();
+    this.input.disabled = false;
+    this.chooseRandomWord();
+    this.lives = 5
+    this.updateLives();
+    this.cleanLetters(); 
   }
 }
 
@@ -83,12 +155,11 @@ class LettersDisplay {
         htmlRef: letterHtml,
         value: letter,
       });
-      this.lettersContainer.appendChild(letterHtml);
-    }
+      this.lettersContainer.appendChild(letterHtml); 
+     }  
 
-    document.body.appendChild(this.lettersContainer);
-  }
-
+    document.body.appendChild(this.lettersContainer);   
+}
   hasLetter(letter) {
     for (const objectLetter of this.letters) {
       if (objectLetter.value === letter) {
