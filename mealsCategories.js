ae11ceb6a4f814aef5c0ee1fa6fe0b742e21f8c4
AndreiMaresDan <andrei.mares911@gmail.com>
2020-08-11 20:19:46 +0300

    homework done

--- mealsCategories.js
+++ mealsCategories.js
@@ -3,28 +3,58 @@ console.log("meals categories");
 class PopUp {
   constructor() {
     this.popHtml = document.getElementById("pop-up");
+    this.popContent=document.getElementById("pop-up-content")
+
     document
       .getElementById("close")
       .addEventListener("click", this.close.bind(this));
+      
   }
 
-  show(mealCatergory) {
+  show(mealCategory) {
     this.popHtml.classList.add("pop-up-show");
-    this.renderMealsList(mealCatergory);
+    this.renderMealsList(mealCategory);
   }
 
-  renderMealsList(mealCatergory) {
+  renderMealsList(mealCategory) {
     fetch(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${mealCatergory}`
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${mealCategory}`
     )
-      .then((response) => response.json())
-      .then((json) => {
+      .then(response => response.json())
+      .then(json => {
         console.log(json);
+        console.log(mealCategory);
+          this.displayMeals(json.meals);
+          console.log(json.meals)
       });
+  }
+
+  displayMeals(mealCategory) {
+
+    for (const meal of mealCategory) {
+      const newCategory = document.createElement("div");
+      newCategory.classList.add("pop-up-div")
+
+      const img = document.createElement("img");
+      img.src = meal.strMealThumb;
+      img.classList.add("pop-up-imgs")
+      newCategory.appendChild(img);
+
+
+      const description = document.createElement("p");
+      description.innerText = meal.strMeal;
+      newCategory.appendChild(description);
+      description.classList.add("text-pop-up")
+
+
+      this.popContent.appendChild(newCategory);
+    }
   }
 
   close() {
     this.popHtml.classList.remove("pop-up-show");
+    this.popContent.innerText=""
+
   }
 }
 
@@ -36,15 +66,15 @@ class Categories {
 
     this.htmlContainer = document.getElementById("meals-categories-container");
     this.getMealsCategories();
-    console.log(this.categories);
   }
 
   getMealsCategories() {
     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-      .then((response) => response.json())
-      .then((json) => {
+      .then(response => response.json())
+      .then(json => {
         this.categories = json.categories;
         this.renderCategories();
+        console.log(this.categories);
       });
   }
 
