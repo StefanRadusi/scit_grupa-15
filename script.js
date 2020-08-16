8ce99c2eea7f97b5d5ede73f4fe7fc2d0aad5aa7
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-08-16 15:32:40 +0300

    homework comments

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1. (mandatory) Create a function that calculate a the sum of numbers from 1 to N.
+
+// 2. (mandatory) Create a function that calculate the value of n to the m power.
+
+// 3. (optional) Create a function that find the maximum number in a data structure.
