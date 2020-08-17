550cdcdcd658cbecaee6573b6738db44645aadb0
renata2014 <x.rena_x@yahoo.com>
2020-08-17 21:01:59 +0300

    done

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // 1. (mandatory) Create a function that calculate a the sum of numbers from 1 to N.
+function sum(n) {
+  if (n < 1) return 0;    
+  return n  + sum(n - 1); 
+}
+
+console.log(sum(3));
+console.log(sum(8));
 
 // 2. (mandatory) Create a function that calculate the value of n to the m power.
+function power(base, exp) {
+  if(exp === 0){
+      return 1;
+  }
+  return base * power(base, exp - 1);
 
-// 3. (optional) Create a function that find the maximum number in a data structure.
+}
+console.log(power(3,4))
+console.log(power(8,7))
+ 
+
+
