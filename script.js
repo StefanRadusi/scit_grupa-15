f7ba75367dafc0a2389779c6f421b1c1a3a821e4
renata2014 <x.rena_x@yahoo.com>
2020-08-17 18:27:16 +0300

    homework done

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // 1. (mandatory) Create a function that calculate a the sum of numbers from 1 to N.
+function Calculate(n){
+    if (n === 0) return 0;
+    let sum = 1;
+    for(let i=1; i < n; i++){
+      sum = sum + (i+1);
+    }
+    return sum;
+  } 
+  console.log(Calculate(7));
+  console.log(Calculate(9));
 
 // 2. (mandatory) Create a function that calculate the value of n to the m power.
+function power(base, exp) {
+  if(exp === 0){
+      return 1;
+  }
+  return base * power(base, exp - 1);
 
-// 3. (optional) Create a function that find the maximum number in a data structure.
+}
+console.log(power(3,4))
+console.log(power(8,7))
+ 
+
+
