bff03c1ca8a6b1dc69a2020fb5d57bb7ed2e06b6
Ale-we <liss_alexandra@yahoo.com>
2020-08-19 19:25:10 +0300

    homework done

--- mealsCategories.js
+++ mealsCategories.js
@@ -5,7 +5,8 @@ class PopUp {
     this.popHtml = document.getElementById("pop-up");
     document
       .getElementById("close")
-      .addEventListener("click", this.close.bind(this));
+      .addEventListener("click", this.close.bind(this)); 
+      
   }
 
   show(mealCatergory) {
@@ -18,11 +19,27 @@ class PopUp {
       `https://www.themealdb.com/api/json/v1/1/filter.php?c=${mealCatergory}`
     )
       .then((response) => response.json())
-      .then((json) => {
+      .then((json)=> {
         console.log(json);
+        this.meals = json.meals;
+        this.displayMealsPopUp(json.meals);
       });
   }
 
+  displayMealsPopUp() {
+    this.cards = [];
+    for (const meal of this.meals) {
+      // console.log(meals);
+      this.cards.push(
+        new DisplayMealsCategoryPopUp(
+          meal.strMeal,
+          meal.strMealThumb,
+          // meal.strMealDescription,
+          this.popHtml
+        )
+      );
+    }
+  }
   close() {
     this.popHtml.classList.remove("pop-up-show");
   }
@@ -99,4 +116,43 @@ class MealCategory {
   }
 }
 
+class DisplayMealsCategoryPopUp {
+  constructor(tile, img,  parent) {
+    this.tile = tile;
+    this.img = img;
+    // this.shortDesc = this.shortDesc;
+    this.parent = parent;
+
+    this.renderPopUp();
+
+  }
+
+    renderPopUp() {
+      const containerPopUp = document.getElementById("pop-up-content");
+     
+     const div = document.createElement("div");
+     containerPopUp.classList.add("meal-category-popUp");
+     containerPopUp.appendChild(div);
+
+      // container.classList.add("meal-category-card");
+      // container.addEventListener("click", () => {
+      //   console.log(this.tile);
+      //   popUp.show(this.tile);
+      // });
+
+      const imgHtml = document.createElement("img");
+      imgHtml.id = "img-pop-up"
+      imgHtml.src = this.img;
+      containerPopUp.appendChild(imgHtml);
+
+      const tileHtml = document.createElement("h3");
+      tileHtml.innerText = this.tile;
+      tileHtml.id = "tile-pop-up";
+      containerPopUp.appendChild(tileHtml);
+  
+      this.parent.appendChild(containerPopUp);
+    }
+  }
+
 new Categories();
+// new DisplayMealsCategoryPopUp();
