6343b7f3da978f5375db9381e13af6825e9873c2
AlexEpu <65967586+AlexEpu@users.noreply.github.com>
2020-08-23 17:01:13 +0300

    homework-done

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-// 1. (mandatory) Create a function that calculate a the sum of numbers from 1 to N.
 
-// 2. (mandatory) Create a function that calculate the value of n to the m power.
+//Create a function that calculate a the sum of numbers from 1 to N
 
-// 3. (optional) Create a function that find the maximum number in a data structure.
+function sumCalculator(n) {
+    if (n == 1) return 1;
+    return n + sumCalculator(n - 1);
+}
+
+console.log(sumCalculator(110));
+console.log(sumCalculator(131));
+console.log(sumCalculator(253));
+console.log(sumCalculator(342));
+console.log(sumCalculator(463));
+
+
+//Create a function that calculate the value of n to the m power
+
+const power = function (a, n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return a * power(a, n - 1);
+    }
+};
+
+console.log(power(4, 2));
+console.log(power(5, 3));
+console.log(power(6, 5));
+console.log(power(7, 6));
