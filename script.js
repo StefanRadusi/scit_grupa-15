1fedd3a8da406e7013ae37f3e434969000fe1c5f
tudorsf <tudor.sfarasan94@gmail.com>
2020-09-12 14:50:24 +0300

    dom-manipulation-homework-done

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
-console.log("start");
+const addButton = document.getElementById("addFiled");
 
-// example of reacting to button click
-const sendButton = document.getElementById("send");
+function onClickAddButton() {
+    const inputField = document.getElementById("fieldName");
 
-// this the function that will be called when click event is triggered
-function onClickSendButton() {
-  console.log("click on", sendButton);
+    if (inputField.value === "") {
+        inputField.style.borderColor = "red";
+        return
+    }
+
+    inputField.style.borderColor = "";
+    const newInput = document.createElement("input");
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("field");
+    const newPara = document.createElement("p");
+
+    document.querySelector(".form-fields").appendChild(newDiv);
+    newDiv.appendChild(newPara);
+    newDiv.appendChild(newInput);
+
+    newPara.innerText = inputField.value;
+
+    newInput.setAttribute("id", inputField.value);
+    inputField.value = "";
 }
 
-// we attached a function to click event triggered by send button
+addButton.addEventListener("click", onClickAddButton)
+
+
+const sendButton = document.getElementById("send");
+
+function onClickSendButton() {
+
+    let formInputs = document.querySelectorAll("input:not(#fieldName)");
+    let myObject = {};
+    let isComplete = true;
+    for (const field of formInputs) {
+        if (!field.value) {
+            field.style.borderColor = "red";
+            isComplete = false;
+        } else {
+            field.style.borderColor = "";
+        }
+        myObject[field.id] = field.value;
+    }
+
+    
+    const finalString = Object.keys(myObject).map(function (key) {
+        let count = ` ${key} - ${myObject[key]}`;
+        return count;
+    }).join(", ");
+    if (isComplete === true) {
+        document.getElementsByClassName("form-container")[0].innerHTML = `sended info: ${finalString}`;
+    }
+    console.log(finalString)
+
+    
+}
+console.log("click on", sendButton);
+
 sendButton.addEventListener("click", onClickSendButton);
