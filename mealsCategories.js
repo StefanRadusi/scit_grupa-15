04a1e2ae786ea1e29bfdcc4f59e1a94432636e8e
tudorsf <tudor.sfarasan94@gmail.com>
2020-09-12 15:59:27 +0300

    homework-done

--- mealsCategories.js
+++ mealsCategories.js
@@ -1,11 +1,10 @@
-console.log("meals categories");
-
 class PopUp {
   constructor() {
     this.popHtml = document.getElementById("pop-up");
     document
       .getElementById("close")
-      .addEventListener("click", this.close.bind(this));
+      .addEventListener("click", this.close.bind(this)); 
+      
   }
 
   show(mealCatergory) {
@@ -18,11 +17,27 @@ class PopUp {
       `https://www.themealdb.com/api/json/v1/1/filter.php?c=${mealCatergory}`
     )
       .then((response) => response.json())
-      .then((json) => {
+      .then((json)=> {
         console.log(json);
+        this.meals = json.meals;
+        this.displayMealsPopUp(json.meals);
       });
   }
 
+  displayMealsPopUp() {
+    this.cards = [];
+    for (const meal of this.meals) {
+     
+      this.cards.push(
+        new DisplayMealsCategoryPopUp(
+          meal.strMeal,
+          meal.strMealThumb,
+          
+          this.popHtml
+        )
+      );
+    }
+  }
   close() {
     this.popHtml.classList.remove("pop-up-show");
   }
@@ -98,5 +113,39 @@ class MealCategory {
     this.parent.appendChild(container);
   }
 }
+
+class DisplayMealsCategoryPopUp {
+  constructor(tile, img,  parent) {
+    this.tile = tile;
+    this.img = img;
+    
+    this.parent = parent;
+
+    this.renderPopUp();
+
+  }
+
+    renderPopUp() {
+      const containerPopUp = document.getElementById("pop-up-content");
+     
+     const div = document.createElement("div");
+     containerPopUp.classList.add("meal-category-popUp");
+     containerPopUp.appendChild(div);
+
+    
+
+      const imgHtml = document.createElement("img");
+      imgHtml.id = "img-pop-up"
+      imgHtml.src = this.img;
+      containerPopUp.appendChild(imgHtml);
+
+      const tileHtml = document.createElement("h3");
+      tileHtml.innerText = this.tile;
+      tileHtml.id = "tile-pop-up";
+      containerPopUp.appendChild(tileHtml);
+  
+      this.parent.appendChild(containerPopUp);
+    }
+  }
 
 new Categories();
