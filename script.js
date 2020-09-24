936cd539a2954592eb98cc1c3763a561cef9a42f
Alexandra V. Onet <email@example.com>
2020-09-24 10:54:00 +0300

    Done

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-console.log("start");
-
-console.log(a);
-// var is hoisted
-var a = 1;
-
-// console.log(b);
-let b = 5;
-
-printFunction();
-// functions are hoisted
-function printFunction() {
-  console.log("this is the function");
+// 1.Create a function that calculate a the sum of numbers from 1 to N.
+function calcNrFromOneTo(Number) {
+  if (Number <= 0) return 0;
+  if (Number === 1) return 1;
+  return Number + calcNrFromOneTo(Number - 1);
+  
 }
+console.log(calcNrFromOneTo(4));
+
+// 2. Create a function that calculate the value of n to the m power.
+function toThePower(Base,Exponential) {
+  if (Base === 0) return 0;
+  if (Exponential === 0) return 1
+  return Base * toThePower(Base, Exponential - 1);
+}
+console.log(toThePower(3,4));
